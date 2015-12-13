8314eae93be7a4bc1119e2230390b10c2b929d60
Mohamed Boudra <boudra.moha@gmail.com>
2015-12-13 23:26:18 +0100

    add timestamp to uri

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const getSignature = require('./signature');
+const http = require('http');
+
+const defaults = {
+    Service: 'AWSECommerceService',
+    Version: '2013-08-01'
+};
 
 var AmazonProducts = Object.create({
 
     addCommonParams(params) {
-        params.Service = 'AWSECommerceService';
+        params.Service = this.Service;
         params.AssociateTag = this.Tag;
         params.AWSAccessKeyId = this.AccessKey;
+        params.Version = this.Version;
+        params.Timestamp = (new Date).toISOString().replace(/\.\d{3}/, '');
         return params;
     },
 
     operation(name, params) {
-        params = params || {};
-        params = this.addCommonParams(params);
-        params.Signature = getSignature(params, this.SecretKey);
+
+        return new Promise((resolve, reject) => {
+
+            params = params || {};
+            params = this.addCommonParams(params);
+            params.Signature = getSignature(params, this.SecretKey);
+
+            const uri = Object.keys(params).map(function(key) {
+                return encodeURIComponent(key) + '=' +
+                    encodeURIComponent(params[key]);
+            }).join('&');
+
+            console.log(uri);
+
+        });
+
     }
 
 });
@@ -24,6 +45,8 @@ module.exports = function(options) {
     if(!options.AccessKey) throw 'The AccessKey is required';
     if(!options.SecretKey) throw 'The SecretKey is required';
     if(!options.Tag)       throw 'The Tag is required';
+
+    options = Object.assign(defaults, options);
 
     return Object.assign(Object.create(AmazonProducts), options);
 };
--- signature.js
+++ signature.js
@@ -2,7 +2,6 @@ const crypto = require('crypto');
 
 module.exports = function(params, secret) {
     const hmac = crypto.createHmac('sha256', secret);
-    // params.Timestamp = (new Date).toISOString().replace(/\.\d{3}/, '');
     const stringToSign = [
         'GET', 'webservices.amazon.com', '/onca/xml',
         Object.keys(params).map(function(key) {
